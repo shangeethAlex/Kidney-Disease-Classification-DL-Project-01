0ebed7892c926f3af0f7639afbcc6af84bc81b30
shangeethAlex <shangeethmathan2020@gmail.com>
2024-07-12 13:34:01 +0530

    common read yaml file updated

--- src/Kidney_Disease_Classifier/utiles/common.py
+++ src/Kidney_Disease_Classifier/utiles/common.py
@@ -13,18 +13,7 @@ import base64
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns
 
-    Args:
-        path_to_yaml (str): path like input
-
-    Raises:
-        ValueError: if yaml file is empty
-        e: empty file
-
-    Returns:
-        ConfigBox: ConfigBox type
-    """
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
@@ -39,18 +28,14 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories
 
-    Args:
-        path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
-    """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
 
 
+#save Json Data
 @ensure_annotations
 def save_json(path: Path, data: dict):
     """save json data
@@ -66,7 +51,7 @@ def save_json(path: Path, data: dict):
 
 
 
-
+#load the json file data
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
     """load json files data
