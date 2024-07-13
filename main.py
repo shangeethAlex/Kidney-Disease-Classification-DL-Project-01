dc359c24c39b0e3fe63f9201862ed6352da80118
shangeethAlex <shangeethmathan2020@gmail.com>
2024-07-13 18:28:35 +0530

    model training pipeline updated into main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from Kidney_Disease_Classifier import logger
 from Kidney_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeling
+from Kidney_Disease_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
+
 try:
     logger.info(f"<<<<<<stage {STAGE_NAME} started >>>>>>>>>")
     obj = DataIngestionTrainingPipeling()
@@ -9,5 +12,20 @@ try:
     logger.info(f">>>>>>>>>>>>>>>{STAGE_NAME} completed<<<<<<<")
 except Exception as e:
     raise e
+
+
+STAGE_NAME = "Prepare base Model"
+
+try:
+    logger.info(">>>>>>>>>preparemodeltraining start<<<<<<<")
+    logger.info(f">>>>stage {STAGE_NAME} started")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f"<<<<<<<<<<stage {STAGE_NAME} completed >>>>>>")
+
+except Exception as e:
+    raise e
+
+        
     
     
